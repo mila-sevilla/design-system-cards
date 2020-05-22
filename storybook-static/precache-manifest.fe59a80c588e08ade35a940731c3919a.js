daf113c67b5e7e5b72b4b3fa7c7c57013955f00e
Mila Sevilla <m.sevillazhou@gmail.com>
2020-05-22 18:57:26 +0200

    feat: :rocket: Deploying to Github pages

---
+++ storybook-static/precache-manifest.fe59a80c588e08ade35a940731c3919a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a30a90735b9aedf6774f372da7a3edd",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.f499ccb361f6db10fcae.bundle.js"
+  },
+  {
+    "url": "runtime~main.f499ccb361f6db10fcae.bundle.js"
+  },
+  {
+    "url": "vendors~main.f499ccb361f6db10fcae.bundle.js"
+  },
+  {
+    "url": "vendors~main.f499ccb361f6db10fcae.bundle.js.LICENSE.txt"
+  }
+]);
